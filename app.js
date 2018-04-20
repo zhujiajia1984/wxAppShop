69a2dec39cad51b10d95d1165c4c923842cf49d8
zhujiajia <772978409@qq.com>
2018-04-20 17:36:46 +0800

    完成用户登录功能

--- app.js
+++ app.js
@@ -1,30 +1,94 @@
 App({
   ///////////////////////////////////////////////////////////////////////////////////////////
-  // 获取用户信息接口
-  getUserInfo: function(cb){
+  // 获取用户信息（已经授权状态下）
+  getUserInfo: function (cb) {
+    let withCredentials = true;
+    if (wx.getStorageSync('userInfo')) {
+      // 已有则不再向服务器发送
+      withCredentials = false;
+    }
+    wx.getUserInfo({
+      withCredentials: withCredentials,
+      success: (res) => {
+        wx.setStorageSync('userInfo', res.userInfo);
+        if (withCredentials) {
+          // 服务器保存用户信息
+          let token = wx.getStorageSync('token');
+          wx.request({
+            url: "https://wechat.weiquaninfo.cn/wxAppLogin/userInfo",
+            method: "POST",
+            header: {
+              'content-type': 'application/json',
+              'authorization': token,
+            },
+            data: {
+              rawData: res.rawData,
+              signature: res.signature,
+              encryptedData: res.encryptedData,
+              iv: res.iv,
+            },
+            success: function (res) {
+              if (res.data.result_msg != "success"){
+                console.log("saveUserInfo failed!");
+              }
+            },
+            fail: function (error) {
+              console.log(error);
+            }
+          })
+        }
+        // 返回
+        typeof cb == "function" && cb({type: "success"});
+      },
+      fail: (error) => {
+        console.log(`wx.getUserInof error:${error}`);
+      }
+    })
+  },
+
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  // 获取用户授权和用户信息
+  getUserAuthAndInfo: function (cb) {
     wx.getSetting({
-      success: (res)=>{
-        if (res.authSetting['scope.userInfo']){
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {
           // 已经授权
-          console.log("已经授权");
-        }else{
+          console.log("已经授权_用户信息");
+          if (wx.getStorageSync('userInfo')){
+            // 返回
+            typeof cb == "function" && cb({ type: "success" });
+          }else{
+            this.getUserInfo(cb);
+          }
+        } else {
           // 未授权
-          console.log("未授权");
+          console.log("未授权_用户信息");
           wx.authorize({
             scope: 'scope.userInfo',
-            success: (res)=>{
+            success: (res) => {
               // 授权成功
-              console.log("授权成功");
+              console.log("授权成功_用户信息");
+              if (wx.getStorageSync('userInfo')) {
+                // 返回
+                typeof cb == "function" && cb({ type: "success" });
+              } else {
+                wx.removeStorageSync('userInfo');
+                this.getUserInfo(cb);
+              }
             },
-            fail: (error)=>{
+            fail: (error) => {
               // 授权失败
-              console.log("授权失败");
-              typeof cb == "function" && cb("auth fail");
+              console.log("授权失败_用户信息");
+              wx.removeStorageSync('userInfo');
+              // 返回授权失败信息
+              typeof cb == "function" && cb({
+                type: "auth fail"
+              });
             }
           })
         }
       },
-      fail: (error)=>{
+      fail: (error) => {
         console.log(`wx.getSetting error:${error}`);
       }
     })
@@ -32,7 +96,8 @@ App({
 
   ///////////////////////////////////////////////////////////////////////////////////////////
   // login接口，发送 res.code 到后台换取 openId, sessionKey, unionId
-  login: function () {
+  login: function (cb) {
+    var that = this;
     wx.login({
       success: (res) => {
         if (res.code) {
@@ -47,6 +112,7 @@ App({
             },
             success: function (res) {
               wx.setStorageSync('token', res.data.token);
+              that.getUserAuthAndInfo(cb); // 用户授权+获取用户信息
             },
             fail: function (error) {
               console.log(error);
@@ -68,11 +134,22 @@ App({
     wx.checkSession({
       success: () => {
         // 登录态未过期
+        let token = wx.getStorageSync('token');
+        if (token) {
+          // 已有token直接获取用户数据
+          this.getUserAuthAndInfo(cb); // 用户授权+获取用户信息
+        } else {
+          // 重新登录
+          wx.removeStorageSync('token');
+          wx.removeStorageSync('userInfo');
+          this.login(cb);
+        }
       },
       fail: () => {
         // 登录态已过期
-        this.login(); //login接口调用
-        this.getUserInfo(cb); // 授权并getUserInfo接口调用
+        wx.removeStorageSync('token');
+        wx.removeStorageSync('userInfo');
+        this.login(cb);
       }
     })
   },
@@ -81,27 +158,27 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    
+
   }
 })
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -8,8 +8,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isAuthed:{
+    isAuthed: {
       isUserInfoAuthed: null,
+    },
+    userInfo: {},
+    commonMenus: [{
+      id: 1,
+      name: "我的订单",
+      src: "/assets/images/order.png"
+    }, {
+        id: 2,
+        name: "购物车",
+        src: "/assets/images/cart.png"
+    }]
+  },
+
+  /////////////////////////////////////////////////////////////////////////////////
+  // 点击菜单事件
+  onMenuClick: function(e){
+    switch(e.currentTarget.dataset.id){
+      case 1:
+        // 我的订单
+        console.log("111");
+        break;
+      case 2:
+        // 购物车
+        console.log("222");
+        break;
+      default:
+        break;
+    }
+  },
+
+  /////////////////////////////////////////////////////////////////////////////////
+  // 点击重新授权按钮事件
+  getUserInfo: function (res) {
+    if (res.detail.errMsg == "getUserInfo:ok") {
+      wx.showNavigationBarLoading();
+      app.userLogin((res) => {
+        // 渲染页面
+        // this.setData({ token: token });
+        if (res.type == "auth fail") {
+          let isAuthed = this.data.isAuthed;
+          isAuthed.isUserInfoAuthed = 'fail';
+          this.setData({ isAuthed: isAuthed });
+        } else if (res.type == "success") {
+          let isAuthed = this.data.isAuthed;
+          isAuthed.isUserInfoAuthed = 'ok';
+          this.setData({ isAuthed: isAuthed });
+        }
+        wx.hideNavigationBarLoading();
+      });
     }
   },
 
@@ -18,14 +67,22 @@ Page({
    */
   onLoad: function (options) {
     // 用户登录
+    wx.showNavigationBarLoading();
     app.userLogin((res) => {
       // 渲染页面
-      // this.setData({ token: token });
-      if (res == "auth fail"){
+      if (res.type == "auth fail") {
+        // 失败页面
         let isAuthed = this.data.isAuthed;
         isAuthed.isUserInfoAuthed = 'fail';
         this.setData({ isAuthed: isAuthed });
+      } else if (res.type == "success") {
+        // 成功页面
+        let userInfo = wx.getStorageSync('userInfo');
+        let isAuthed = this.data.isAuthed;
+        isAuthed.isUserInfoAuthed = 'ok';
+        this.setData({ isAuthed: isAuthed, userInfo: userInfo});
       }
+      wx.hideNavigationBarLoading();
     });
   },
 
@@ -33,48 +90,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
