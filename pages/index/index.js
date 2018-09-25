dafc3956ae040ffcfe46438ecd7ddb4e68c5ce35
zhujiajia2003 <zhujiajia2003@163.com>
2018-09-25 11:00:56 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,62 +4,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
+  },
+
+  //事件处理函数
+  bindViewTap: function () {
+    wx.addCard({
+      cardList: [{
+        cardId: 'pMBhJ0g_felGMMBBDwrgDhx0FjyQ',
+        cardExt: '{"timestamp": "1530088010", "signature":"691adc8c5c6891e3b30d7e6a3e2c8939c8211d4c", "nonce_str":"zjj15202185066"}'
+      }],
+      success: function (res) {
+        console.log(res.cardList) // 卡券添加结果
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
